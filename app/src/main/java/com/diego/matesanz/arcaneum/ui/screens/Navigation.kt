f1c2f420590759d27cc554540c858a5fc0c7e6c8
Diego Matesanz <diegomatesanzfuente@gmail.com>
2024-12-08 18:43:14 +0100

    Type safe navigation

--- app/src/main/java/com/diego/matesanz/arcaneum/ui/screens/Navigation.kt
+++ app/src/main/java/com/diego/matesanz/arcaneum/ui/screens/Navigation.kt
@@ -2,44 +2,55 @@ package com.diego.matesanz.arcaneum.ui.screens
 
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
+import androidx.navigation.toRoute
+import com.diego.matesanz.arcaneum.ui.screens.Screen.Camera
+import com.diego.matesanz.arcaneum.ui.screens.Screen.Detail
+import com.diego.matesanz.arcaneum.ui.screens.Screen.Home
 import com.diego.matesanz.arcaneum.ui.screens.camera.CameraScreen
 import com.diego.matesanz.arcaneum.ui.screens.detail.DetailScreen
 import com.diego.matesanz.arcaneum.ui.screens.detail.DetailViewModel
 import com.diego.matesanz.arcaneum.ui.screens.home.HomeScreen
+import kotlinx.serialization.Serializable
 
 @Composable
 fun Navigation() {
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = "home") {
-        composable("home") {
+    NavHost(navController = navController, startDestination = Home) {
+        composable<Home> {
             HomeScreen(
-                onBookClick = { book -> navController.navigate("detail/${book.id}") },
-                onCamClick = { navController.navigate("camera") },
-                onBookmarked = { book ->  },
+                onBookClick = { book -> navController.navigate(Detail(book.id)) },
+                onCamClick = { navController.navigate(Camera) },
+                onBookmarked = { book -> },
             )
         }
-        composable(
-            route = "detail/{bookId}",
-            arguments = listOf(navArgument("bookId") { type = NavType.StringType })
-        ) { backStackEntry ->
-            val bookId = requireNotNull(backStackEntry.arguments?.getString("bookId"))
+        composable<Detail> { backStackEntry ->
+            val detail: Detail = backStackEntry.toRoute()
             DetailScreen(
-                viewModel = viewModel { DetailViewModel(bookId) },
+                viewModel = viewModel { DetailViewModel(detail.bookId) },
                 onBack = { navController.popBackStack() },
-                onBookmarked = { book ->  },
+                onBookmarked = { book -> },
             )
         }
-        composable("camera") {
+        composable<Camera> {
             CameraScreen(
                 onBack = { navController.popBackStack() },
-                onBookClick = { book -> navController.navigate("detail/${book.id}") },
+                onBookClick = { book -> navController.navigate(Detail(book.id)) },
             )
         }
     }
+}
+
+private sealed class Screen {
+    @Serializable
+    object Home
+
+    @Serializable
+    data class Detail(val bookId: String)
+
+    @Serializable
+    object Camera
 }
